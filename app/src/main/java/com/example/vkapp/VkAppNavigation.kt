7360abc5f5aaa54972c400714ae4b904f935e46b
amir.elkanov <amirelkanov0504@gmail.com>
2026-03-10 12:45:24 +0300

    Nav constant fix

--- app/src/main/java/com/example/vkapp/VkAppNavigation.kt
+++ app/src/main/java/com/example/vkapp/VkAppNavigation.kt
@@ -24,7 +24,7 @@ fun VkAppNavHost(
         composable(AppList.route) {
             AppListScreen(
                 onAppClick = {
-                    navController.navigate("app_details")
+                    navController.navigate(AppDetails.route)
                 },
             )
         }
